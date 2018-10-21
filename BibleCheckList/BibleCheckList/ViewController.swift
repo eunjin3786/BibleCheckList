10c6912fcb3ab80a277614ecfd284e48b7e5c291
eunjin Jo <eunjin3786@naver.com>
2018-10-22 03:23:21 +0900

    Cocoapod & Realm설치

---
+++ BibleCheckList/BibleCheckList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BibleCheckList
+//
+//  Created by eunjin Jo on 2018. 10. 22..
+//  Copyright © 2018년 eunjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
