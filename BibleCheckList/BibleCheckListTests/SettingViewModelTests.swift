7bd802480880681ba3c7e575b9527a91596b4ab3
eunjin Jo <eunjin3786@naver.com>
2019-04-13 22:58:52 +0900

    SettingViewModelTests 파일 생성

---
+++ BibleCheckList/BibleCheckListTests/SettingViewModelTests.swift
@@ -0,0 +1,14 @@
+//
+//  SettingViewModelTests.swift
+//  BibleCheckListTests
+//
+//  Created by eunjin Jo on 13/04/2019.
+//  Copyright © 2019 eunjin. All rights reserved.
+//
+
+import XCTest
+@testable import BibleCheckList
+
+class SettingViewModelTests: XCTestCase {
+
+}
